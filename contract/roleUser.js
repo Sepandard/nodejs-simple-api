48caa7ab2e67f8ca7641457a43f1ff3c3fafae45
Sepandard <Sepandard@systemgroup.net>
2023-01-02 03:52:41 +0330

    apply contract

---
+++ contract/roleUser.js
@@ -0,0 +1,6 @@
+const RoleUser = {};
+
+RoleUser.ADMIN = 1;
+RoleUser.USER = 2;
+
+module.exports = RoleUser;
---
+++ contract/statusUser.js
@@ -0,0 +1,7 @@
+const StatusUser = {};
+
+StatusUser.INACTIVE = 1;
+StatusUser.ACTIVE = 2;
+StatusUser.BANNED = 3;
+
+module.exports = StatusUser;
